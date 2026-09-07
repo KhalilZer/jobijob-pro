e531a5c436aebb8d0ac02beebf1050ddc81d874c
Khalil Zerzour <khalilzerzour100@gmail.com>
2026-09-07 14:27:22 +0100

    Feat : create GatewayAPi and server Eureka

---
+++ api-gateway-svc/src/test/java/com/jobipro/apigatewaysvc/ApiGatewaySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobipro.apigatewaysvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewaySvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
